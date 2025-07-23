683dd8dfaee6b68ff206137c0dbdb8737bc2745f
DianaErmakova <ermakovadin64@gmail.com>
2025-07-23 17:35:18 +0300

    Исправила ошибки, связанные с flake8 попытка4 исправила файл predict_utils.py

--- src/predict_utils.py
+++ src/predict_utils.py
@@ -9,13 +9,21 @@ MODEL_PATH = os.path.join(BASE_DIR, '..', 'models', 'model.joblib')
 SCALER_PATH = os.path.join(BASE_DIR, '..', 'models', 'scaler.joblib')
 FEATURES_PATH = os.path.join(BASE_DIR, '..', 'models', 'features.joblib')
 
-# Загрузка
-model = joblib.load(MODEL_PATH)
-scaler = joblib.load(SCALER_PATH)
-features = joblib.load(FEATURES_PATH)
+# Проверка и загрузка
+model = None
+scaler = None
+features = None
+
+if os.path.exists(MODEL_PATH) and os.path.exists(SCALER_PATH) and os.path.exists(FEATURES_PATH):
+    model = joblib.load(MODEL_PATH)
+    scaler = joblib.load(SCALER_PATH)
+    features = joblib.load(FEATURES_PATH)
 
 
 def predict_from_dict(data: dict):
+    if model is None or scaler is None or features is None:
+        raise RuntimeError("Модель или артефакты не найдены. Убедитесь, что они обучены и сохранены.")
+
     df = pd.DataFrame([data])
     X, _ = preprocess_data(df)
     X = X.reindex(columns=features, fill_value=0)
